7bde631aa12578b5e01ba3ad50e8008b1881af36
Anagh Kanungo <23740251+anaghkanungo7@users.noreply.github.com>
2022-05-02 17:01:08 -0400

    Update JPX.py

--- JPX.py
+++ JPX.py
@@ -15,6 +15,26 @@ from sklearn.linear_model import LinearRegression
 # Polynomial Regression
 from sklearn.preprocessing import PolynomialFeatures
 
+# SVR
+from sklearn.svm import SVR
+import matplotlib.dates as mdates
+
+# LSTM
+# import tensorflow
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.layers import LSTM
+# from tensorflow.keras.layers import Dropout
+# from tensorflow.keras.layers import *
+# from tensorflow.keras.callbacks import EarlyStopping
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import mean_squared_error
+
 
 # Multiple Linear Regression
 from sklearn.model_selection import train_test_split
@@ -29,15 +49,16 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 from sklearn.cluster import KMeans
+from sqlalchemy import false
 
 path = "dataset/train_files/stock_prices.csv"
 df = pd.read_csv(path)
 
 
-print(df.size)
+# print(df.size)
 # df = df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
 df = df[df['High'].notna()]
-print(df.size)
+# print(df.size)
 
 
 # print(df.head)
@@ -118,145 +139,191 @@ def plotData(df):
     mplfinance.plot(df2)
 
 
-def multipleLinearRegression(df):
-    data = df[['Open', 'High', 'Low', 'Volume', 'Close']]
-    print(data.head())
+# def multipleLinearRegression(df):
+#     data = df[['Open', 'High', 'Low', 'Volume', 'Close']]
+#     print(data.head())
 
-    # X = np.column_stack(
-    #     [np.ones(len(df['x'])), df["x"].values.reshape(1, -1)[0]])
+#     # X = np.column_stack(
+#     #     [np.ones(len(df['x'])), df["x"].values.reshape(1, -1)[0]])
 
-    data_X = data.loc[:, data.columns != 'Close']
-    # data_X = np.column_stack(
-    #     [np.ones(len(data_X['Open'])), data_X.values.reshape(1, -1)[0]])
+#     data_X = data.loc[:, data.columns != 'Close']
+#     # data_X = np.column_stack(
+#     #     [np.ones(len(data_X['Open'])), data_X.values.reshape(1, -1)[0]])
 
-    test2 = data_X["Open"].values.reshape(1, -1)[0]
-    test1 = np.ones(len(data_X['Open']))
-    data_X = np.column_stack(
-        [test1, test2, data_X['High'].values.reshape(1, -1)[0], data_X['Low'].values.reshape(1, -1)[0], data_X['Volume'].values.reshape(1, -1)[0]])
+#     test2 = data_X["Open"].values.reshape(1, -1)[0]
+#     test1 = np.ones(len(data_X['Open']))
+#     data_X = np.column_stack(
+#         [test1, test2, data_X['High'].values.reshape(1, -1)[0], data_X['Low'].values.reshape(1, -1)[0], data_X['Volume'].values.reshape(1, -1)[0]])
 
-    print(test1.shape)
-    print(test2.shape)
+#     print(test1.shape)
+#     print(test2.shape)
 
-    data_Y = data['Close']
-    # data_Y = data_Y.values.reshape(1, -1)
+#     data_Y = data['Close']
+#     # data_Y = data_Y.values.reshape(1, -1)
 
-    print(data_X.shape)
-    # print(data_X)
-    print(data_Y.shape)
-    # print(data_Y)
+#     print(data_X.shape)
+#     # print(data_X)
+#     print(data_Y.shape)
+#     # print(data_Y)
 
-    train_X, test_X, train_y, test_y = train_test_split(
-        data_X, data_Y, test_size=0.5)
+#     train_X, test_X, train_y, test_y = train_test_split(
+#         data_X, data_Y, test_size=0.5)
 
-    print('\n\nTraining Set')
+#     print('\n\nTraining Set')
 
-    # Creating the Regressor
-    regressor = LinearRegression()
-    regressor.fit(train_X, train_y)
+#     # Creating the Regressor
+#     regressor = LinearRegression()
+#     regressor.fit(train_X, train_y)
 
-    # Make Predictions and Evaluate the results
-    predict_y = regressor.predict(test_X)
-    print('Prediction Score : ', regressor.score(test_X, test_y))
-    error = mean_squared_error(test_y, predict_y)
-    print('Mean Squared Error : ', error)
+#     # Make Predictions and Evaluate the results
+#     predict_y = regressor.predict(test_X)
+#     print('Prediction Score : ', regressor.score(test_X, test_y))
+#     error = mean_squared_error(test_y, predict_y)
+#     print('Mean Squared Error : ', error)
 
-    coeff = regressor.coef_
-    print("Coefficients: ", coeff)
+#     coeff = regressor.coef_
+#     print("Coefficients: ", coeff)
 
-    # Plot the predicted and the expected values
-    # fig = plt.figure()
-    # ax = plt.axes()
-    # ax.grid()
-    # ax.set(xlabel='Close ($)', ylabel='Open ($)',
-    #        title='Panasonic Multiple Regression')
-    # ax.plot(test_X['Open'], test_y)
-    # ax.plot(test_X['Open'], predict_y)
-    # fig.savefig('LRPlot.png')
-    # plt.show()
+#     # Plot the predicted and the expected values
+#     # fig = plt.figure()
+#     # ax = plt.axes()
+#     # ax.grid()
+#     # ax.set(xlabel='Close ($)', ylabel='Open ($)',
+#     #        title='Panasonic Multiple Regression')
+#     # ax.plot(test_X['Open'], test_y)
+#     # ax.plot(test_X['Open'], predict_y)
+#     # fig.savefig('LRPlot.png')
+#     # plt.show()
 
-    return coeff, predict_y
-
-
-def simpleLinearRegression(df):
-    dates = []
-    for x in range(0, len(df["Date"])):
-        dates.append(x)
-    prices = df['Close']
-
-    dates = np.asanyarray(dates)
-    prices = np.asanyarray(prices)
-    dates = np.reshape(dates, (len(dates), 1))
-    prices = np.reshape(prices, (len(prices), 1))
-
-    xtrain, xtest, ytrain, ytest = train_test_split(
-        dates, prices, test_size=0.2)
-    # best = reg.score(ytrain, ytest)
-    best = 0
-    # bestReg
-    for _ in range(100):
-        xtrain, xtest, ytrain, ytest = train_test_split(
-            dates, prices, test_size=0.2)
-        reg = LinearRegression().fit(xtrain, ytrain)
-        acc = reg.score(xtest, ytest)
-        if acc > best:
-            best = acc
-            bestReg = reg
-
-    mean = 0
-    for i in range(10):
-        msk = np.random.rand(len(df)) < 0.8
-        xtest = dates[~msk]
-        ytest = prices[~msk]
-        mean += bestReg.score(xtest, ytest)
-
-    print("Average Accuracy: ", mean/10)
-
-    print("R^2 Score: ", bestReg.score(dates, prices))
-
-    # Plot Predicted VS Actual Data
-    plt.plot(xtest, ytest, color='green', linewidth=1,
-             label='Actual Price')  # plotting the initial datapoints
-    plt.plot(xtest, bestReg.predict(xtest), color='blue', linewidth=3,
-             label='Predicted Price')  # plotting the line made by linear regression
-    plt.title('Linear Regression | Time vs. Price ')
-    plt.legend()
-    plt.xlabel('Date Integer')
-    plt.show()
+#     return coeff, predict_y
 
 
-def polynomialRegression(df):
-    dates = []
-    for x in range(0, len(df["Date"])):
-        dates.append(x)
+# def simpleLinearRegression(df):
+#     dates = []
+#     for x in range(0, len(df["Date"])):
+#         dates.append(x)
+#     prices = df['Close']
 
-    dates = np.array(dates)
+#     dates = np.asanyarray(dates)
+#     prices = np.asanyarray(prices)
+#     dates = np.reshape(dates, (len(dates), 1))
+#     prices = np.reshape(prices, (len(prices), 1))
 
-    prices = df['Close']
+#     xtrain, xtest, ytrain, ytest = train_test_split(
+#         dates, prices, test_size=0.2)
+#     # best = reg.score(ytrain, ytest)
+#     best = 0
+#     # bestReg
+#     for _ in range(100):
+#         xtrain, xtest, ytrain, ytest = train_test_split(
+#             dates, prices, test_size=0.2)
+#         reg = LinearRegression().fit(xtrain, ytrain)
+#         acc = reg.score(xtest, ytest)
+#         if acc > best:
+#             best = acc
+#             bestReg = reg
 
-    for i in range(2, 5):
-        poly = PolynomialFeatures(degree=i, include_bias=False)
-        poly_features = poly.fit_transform(dates.reshape(-1, 1))
-        xtrain, xtest, ytrain, ytest = train_test_split(
-            poly_features, prices, test_size=0.2)
+#     mean = 0
+#     for i in range(10):
+#         msk = np.random.rand(len(df)) < 0.8
+#         xtest = dates[~msk]
+#         ytest = prices[~msk]
+#         mean += bestReg.score(xtest, ytest)
 
-        poly_reg_model = LinearRegression().fit(xtrain, ytrain)
-        y_predicted = poly_reg_model.predict(poly_features)
+#     print("Average Accuracy: ", mean/10)
 
-        print("R^2 Score: ", poly_reg_model.score(poly_features, prices))
+#     print("R^2 Score: ", bestReg.score(dates, prices))
 
-        plt.title("Polynomial Regression", size=16)
-        plt.plot(dates, prices, '-')
-        plt.plot(dates, y_predicted, c="red")
-        plt.show()
+#     # Plot Predicted VS Actual Data
+#     # plt.plot(xtest, ytest, color='green', linewidth=1,
+#     #          label='Actual Price')  # plotting the initial datapoints
+#     # plt.plot(xtest, bestReg.predict(xtest), color='blue', linewidth=3,
+#     #          label='Predicted Price')  # plotting the line made by linear regression
+#     # plt.title('Linear Regression | Time vs. Price ')
+#     # plt.legend()
+#     # plt.xlabel('Date Integer')
+#     # plt.show()
+
+#     return bestReg.score(dates, prices)
+
+
+# def polynomialRegression(df):
+#     dates = []
+#     for x in range(0, len(df["Date"])):
+#         dates.append(x)
+
+#     dates = np.array(dates)
+
+#     prices = np.array(df['Close'])
+
+#     x_train, x_test, y_train, y_test = train_test_split(
+#         dates, prices, test_size=0.2)  # splits the dataset into 80-20 random points
+
+#     print(x_train[0:5])
+#     print(x_test[0:5])
+
+#     x_train = np.array(x_train)
+#     y_train = np.array(y_train)
+#     x_test = np.array(x_test)
+#     y_test = np.array(y_test)
+
+#     x_train = x_train.reshape(-1, 1)
+#     y_train = y_train.reshape(-1, 1)
+
+#     y_train = y_train[x_train[:, 0].argsort()]
+#     x_train = x_train[x_train[:, 0].argsort()]
+
+#     # x_test = x_test.reshape(-1, 1)
+#     y_test = y_test.reshape(-1, 1)
+
+#     myList = np.array([])
+#     for i in range(2, 21):
+#         # Transform polynomial features
+#         poly = PolynomialFeatures(degree=i, include_bias=False)
+#         x_poly = poly.fit_transform(x_train)
+
+#         # Train model
+#         poly_reg_model = LinearRegression().fit(x_poly, y_train)
+
+#         # Predict
+#         y_predicted = poly_reg_model.predict(x_test_poly)
+
+#         # Training error
+#         score = poly_reg_model.score(x_poly, y_train)
+#         # print("R^2 Score: ", score)
+#         myList = np.append(myList, score)
+
+#         # Plot data
+
+#         # plt.title("Polynomial Regression", size=16)
+#         plt.scatter(x_train, y_train, c='green')
+#         plt.scatter(x_test, y_test, c='red')
+#         # plt.plot(x_train, y_predicted, c="red")
+#         # plt.show()
+
+#     return myList
 
 
 # Fetch data for Panasonic
-df_6752 = fetchCompanyData(6752, df)
+# df_6752 = fetchCompanyData(6752, df)
 # plotData(df_6752)
 
-simpleLinearRegression(df_6752)
-polynomialRegression(df_6752)
+# r2score = simpleLinearRegression(df_6752)
+# print(r2score)
 
+# myList = polynomialRegression(df_6752)
+# myList = np.insert(myList, 0, r2score)
+# print(myList)
+# # myList = myList * 100
+# myList2 = []
+# for x in range(1, 21):
+#     myList2.append(x)
+
+
+# plt.bar(myList2, myList, color='green')
+# plt.xlabel('Degree')
+# plt.ylabel('R^2 Score')
+# plt.show()
 
 # Plot data
 # plotData(df_6752)
@@ -269,3 +336,116 @@ polynomialRegression(df_6752)
 # masks = mask0 | mask1
 # targets = df.loc[masks]
 # print(targets)
+
+
+# --- NEW ---
+# Fetch company data for Panasonic
+df_6752 = fetchCompanyData(6752, df)
+
+# SVR
+
+
+# def SVR(df2):
+#     df = df2[['Close']]
+#     forecast_period = 30
+#     df['Prediction'] = df[['Close']].shift(-forecast_period)
+#     print(df.tail())
+
+#     # Create numpy array of predictions
+#     X = np.array(df.drop(['Prediction'], 1))
+
+#     # Remove last 30 rows
+#     X = X[:-forecast_period]
+#     print(X[0:5])
+
+#     y = np.array(df['Prediction'])
+#     y = y[:-forecast_period]
+#     print(y[0:5])
+
+#     xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2)
+
+#     # SVR
+#     svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+#     svr_rbf.fit(xtrain, ytrain)
+
+#     svm_confidence = svr_rbf.score(xtest, ytest)
+#     print("SVM Confidence: ", svm_confidence)
+
+
+def normalize_data(df):
+    # df on input should contain only one column with the price data (plus dataframe index)
+    min = df.min()
+    max = df.max()
+    x = df
+
+    # time series normalization part
+    # y will be a column in a dataframe
+    y = (x - min) / (max - min)
+    return y
+
+
+def normalizeAndPlotData(df, list_of_companies):
+    for i in range(0, len(list_of_companies) - 5):
+        x = list_of_companies[i]
+        df_single = fetchCompanyData(x['SecuritiesCode'], df)
+        df_single.index = pd.to_datetime(df_single['Date'])
+        df_single['norm'] = normalize_data(df_single['Close'])
+        plt.plot(df_single['Date'], df_single['norm'])
+        plt.legend(str(x['Name']), )
+
+    plt.show()
+
+
+# normalizeAndPlotData(df, list_of_companies)
+
+
+def calculateSVR(df):
+    dates_df = df.copy()
+    # dates_df = dates_df.reset_index()
+    # print(dates_df.head())
+    org_dates = dates_df['Date']
+    print(dates_df['Date'])
+    print(dates_df['Date'].shape)
+    dates_df['Date'] = pd.to_datetime(dates_df['Date'])
+    dates_df['Date'] = dates_df['Date'].map(mdates.date2num)
+    # dates = []
+    # for x in range(0, len(df["Date"])):
+    #     dates.append(x)
+
+    # dates_df = pd.DataFrame(np.hstack([dates, dates_df['Date']]))
+
+    dates = np.array(dates_df['Date'])
+    prices = np.array(df['Close'])
+
+    # Convert to 1D Vector
+    dates = np.reshape(dates, (len(dates), 1))
+    prices = np.reshape(prices, (len(prices), 1))
+
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    # dates = dates.reshape(-1, 1)
+    svr_rbf.fit(dates, prices.ravel())
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(dates, prices, color='black', label='Data')
+    plt.plot(org_dates, svr_rbf.predict(dates), color='red', label='RBF model')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+
+
+calculateSVR(df_6752)
+
+
+def SVR2(df):
+    actual_price = df.tail()
+    df = df.head(len(df) - 1)
+
+    dates = []
+    close = []
+
+    df_days = df.loc[:, 'Date']
+    df_close = df.loc[:, 'Close']
+
+    for day in df_days:
+        days.append([])
